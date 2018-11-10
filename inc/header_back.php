47174bc3d3b7dc1950cb33ad49b4f726336a2250
LumberShad <bat76640@gmail.com>
2018-11-10 23:19:19 +0100

    testfavi

--- inc/header_back.php
+++ inc/header_back.php
@@ -3,8 +3,8 @@
 
 <head>
   <meta charset="utf-8" />
-  <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
-  <link rel="icon" type="image/png" href="../assets/img/favicon.png">
+  <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/favicon.ico">
+  <link rel="icon" type="image/png" href="../assets/img/favicon.ico">
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
   <title>
     <?php echo $pagename ?>
@@ -15,6 +15,7 @@
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
   <!-- CSS Files -->
   <link href="assets/css/material-dashboard.css?v=2.1.0" rel="stylesheet" />
+
 </head>
 
 <body class="dark-edition">

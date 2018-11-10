5f81f968e857ed8d6fc87eb66c4a03dd2ac35369
Nicolas Dufresne <nicoladufresne@gmail.com>
2018-11-10 16:51:47 +0100

    git

--- inc/footer.php
+++ inc/footer.php
@@ -1,5 +1,19 @@
-<footer>
-
+<footer id="footer">
+  <section class='wrap'>
+    <div class="section3">
+      <img src="images/mini_TEST2.png" alt="">
+      <h3>Nos réseaux:</h3>
+      <a href="#" class='bouton1'></a>
+      <a href="#" class='bouton2'></a>
+    </div>
+    <div class="section3">
+      <p class="derniere_phrase">© Copyright InfoVaccins® Tous droits réservés</p>
+    </div>
+    <div class="section3">
+      <h3>Notre adresse:</h3>
+      <p>24 Place Saint-Marc <br>76000 Rouen</p>
+    </div>
+  </section>
 </footer>
 </body>
 </html>
--- inc/header.php
+++ inc/header.php
@@ -1,11 +1,55 @@
 <!DOCTYPE html>
-<html lang="fr" >
+<html lang="fr">
   <head>
     <meta charset="utf-8">
-    <link rel="stylesheet" href="assets/css/style.css">
-    <title>Vaccin</title>
+    <meta name="description" content="en-tête">
+    <meta name="author" content="Dufresne Nicolas">
+    <meta name="robots" content="all|(no)follow|(no)index|none">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>InfosVaccins.com</title>
+    <link rel="stylesheet" href="css/style.css">
   </head>
-  <body>
-    <header>
 
-    </header>
+<!-- Le header contient la navbar, le logo, la liste cliquable et l'intro-->
+  <header id="header">
+    <!-- <h1>InfoVaccins.com</h1> -->
+
+      <div class="medicalFond">
+        <div class="navbar">
+          <nav>
+            <ul>
+              <li><a href="#">Acceuil</a></li>
+              <li><a href="#">FAQ</a></li>
+              <li><a href="#">Nous contacter</a></li>
+              <li><a href="#">Se connecter</a></li>
+              <li><a href="#">S'inscrire</a></li>
+            </ul>
+          </nav>
+        </div>
+        <a href="#"><img src="images/logoAcceuil.png" alt="Logo InfosVaccin.com"></a>
+        <div class="clear"></div>
+
+        <div class="intro">
+          <p>Bienvenu sur notre site InfoVaccins.com </br>
+            Si vous souhaitez consulter vos informations concernant vos vaccins, </br>
+            inscrivez-vous dès maintenant !</p>
+
+          <div class="wrap">
+            <!-- Bouton inscription -->
+            <div class="inscription">
+              <a href="#">inscription</a>
+            </div>
+
+            <!-- Bouton connexion -->
+            <div class="connexion">
+              <a href="#">connexion</a>
+            </div>
+          </div>
+
+          <div class="clear"></div>
+
+        </div>
+      </div>
+      </header>
+    <div class="clear"></div>
+  <body>

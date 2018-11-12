03d375f0858fd36f45f9dc3527c5ca3805604ab2
Druxys <drudrux@gmail.com>
2018-11-12 11:27:59 +0100

    password_forget

--- password_forget.php
+++ password_forget.php
@@ -14,7 +14,7 @@ if (!empty($_POST['submitted']))
   $mail = trim(strip_tags($_POST['mail']));
   if (!empty($mail)){
     // Requête SQL des pseudos
-    $sql = "SELECT email, token FROM user WHERE email = :mail";
+    $sql = "SELECT email, token FROM v4_user WHERE email = :mail";
     $query = $pdo -> prepare($sql);
     $query -> bindValue(':mail',$mail);
     $query -> execute();
@@ -24,7 +24,7 @@ if (!empty($_POST['submitted']))
     }
     else {
       if (!empty($user)) {
-         $body = '<p>Cliquez <a href="http://localhost/vaccination4/new_password.php?email='.urlencode($user['email']).'&token='.urlencode($user['token']).'">ICI<?php"></a></p>';
+         $body = '<p>Cliquez <a href=new_password.php?email='.urlencode($user['email']).'&token='.urlencode($user['token']).'">ICI<?php"></a></p>';
         // echo $body;
         mail($mail, "Réinitialisation mot de passe!", $body, $header);
         $success = '<div class="alert-success" role="alert">

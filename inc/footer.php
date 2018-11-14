a6c634e3510a39511a47bd52dd6fc29253751283
Druxys <drudrux@gmail.com>
2018-11-14 15:51:59 +0100

    footer

--- inc/footer.php
+++ inc/footer.php
@@ -1,12 +1,12 @@
 <footer id="footer">
-<div class='wrap'>
+<div class='wrap_footer'>
   <div class="column1">
 				<ul class="footer__menu">
 					<h4>Le Carnet</h4>
           <li><a  href="#" title="Qui sommes-nous ?">Qui sommes-nous ?</a></li>
         <li><a href="#" title="Comment ça marche ?">Comment ça marche ?</a></li>
         <li><a href="#" title="Parutions presse">Parutions presse</a></li>
-        <li><a href=href="#" title="Mentions légales">Mentions légales</a></li>
+        <li><a href="#" title="Mentions légales">Mentions légales</a></li>
 				</ul>
 			</div>
       <div class="column1">
@@ -37,9 +37,7 @@
 </div>
 <div class="clear"></div>
 
-    <div class="column">
-      <p>Nos réseaux sociaux</p>
-  <div class='wrap'>
+  <div class='wrap_footer'>
     <div class="column2">
       <h4>Nos réseaux sociaux</h4>
       <ul class="social-icons">
@@ -61,7 +59,7 @@
   </div>
   <div class="clear"></div>
 
-  <div class='wrap'>
+  <div class='wrap_footer'>
     <div class="column3">
       <p>© Copyright InfoVaccins 2018 ®. All rights reserved.</p>
     </div>

4afdda0520cd928a8e5b66f10d2f3ccf706c91e4
Nicolas Dufresne <nicoladufresne@gmail.com>
2018-11-14 16:15:31 +0100

    wow

--- index.php
+++ index.php
@@ -142,8 +142,8 @@
       }
    ?>
 
-   <h2>Nous contacter</h2>
    <div id="form-contact">
+     <h2>Nous contacter</h2>
         <div class="ligne"></div>
         <div class="wrap">
         <p> Si vous souhaitez nous contacter pour des informations complémentaires, remplissez le formulaire suivant :</p>

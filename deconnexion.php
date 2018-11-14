9fcd3e30a8d6f3326ec23ce51042c4d84fda04d9
BaptisteAngot <bat76640@gmail.com>
2018-11-14 14:47:16 +0100

    push

--- deconnexion.php
+++ deconnexion.php
@@ -1,5 +1,12 @@
 <?php
+include('inc/function.php');
+include('inc/pdo.php');
+include('inc/request.php');
 session_start();
+
+//Deconnecte le user dans la BDD
+changestatus($_SESSION['user'],"deconnecte");
+
 $_SESSION = array();
 session_destroy();
 header('Location: index.php');

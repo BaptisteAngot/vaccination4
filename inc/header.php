6cfb1f581b53924bb17313762c50aa4eb064c2af
Druxys <drudrux@gmail.com>
2018-11-15 10:27:16 +0100

    header

--- inc/header.php
+++ inc/header.php
@@ -10,7 +10,6 @@
     <meta name="author" content="vaccination4">
     <meta name="robots" content="all|(no)follow|(no)index|none">
     <title>InfosVaccins.com</title>
-    <link rel="stylesheet" href="assets/css/style.css">
     <link rel="stylesheet" href="./assets/css/style.css">
     <link rel="stylesheet" href="./assets/flexslider/flexslider.css" type="text/css">
     <link rel="stylesheet" href="./assets/slicknav/slicknav.css" />
